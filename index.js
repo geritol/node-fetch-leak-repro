a24851fa4f61c45905ead720698c49acdc6f0c95
geritol <gergo.tolnai@gmail.com>
2018-03-09 11:11:07 +0100

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch');
+
+async function checkEndpoint(endpointURL) {
+	let fetchResult;
+	try {
+		fetchResult = await fetch(endpointURL);
+	} catch (err) {
+		// Err
+	}
+	return fetchResult;
+}
+
+function checkEndpoints(){
+	const endpoints = [
+		'https://google.com',
+		'https://www.facebook.com/',
+		'https://www.messenger.com/',
+		'https://github.com/',
+		'https://slack.com/',
+		'https://www.wikipedia.org/',
+		'https://www.reddit.com/',
+		'https://news.ycombinator.com/',
+		'https://www.amazon.com/',
+		'https://stackoverflow.com/',
+		'https://www.linkedin.com/',
+		'https://www.npmjs.com/'
+	];
+	endpoints.forEach( endpoint => checkEndpoint(endpoint));
+}
+
+setInterval(checkEndpoints, 1000);
